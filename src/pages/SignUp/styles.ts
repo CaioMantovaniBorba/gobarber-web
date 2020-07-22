8f5ade5c46cda973ddb01ddd147438152c5a0748
Caio Mantovani Borba <caiovmborba@gmail.com>
2020-07-22 00:03:18 -0300

    Sign up page

---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  place-content: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+  }
+
+  > a {
+    display: flex;
+    align-items: center;
+    color: #f4efe8;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#f4efe8')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signUpBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
