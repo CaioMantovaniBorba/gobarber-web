c37d2f508c4a7ec0dbeea89c8afe605fb621cf10
Caio Mantovani Borba <caiovmborba@gmail.com>
2021-01-17 18:01:05 -0300

    Next appointment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -29,6 +29,27 @@ const Dashboard: React.FC = () => {
         </button>
       </HeaderContent>
     </Header>
+
+    <Content>
+      <Schedule>
+        <h1>Horários agendados</h1>
+        <p>
+          <span>Hoje</span>
+          <span>Dia 17</span>
+          <span>Domingo</span>
+        </p>
+
+        <NextAppointment>
+          <strong>Atendimento a seguir</strong>
+          <div>
+            <img src="https://avatars2.githubusercontent.com/u/38335770?s=460&u=a5b1e9ff1a5b094e518862d3a098a880e753b443&v=4" alt="Caio Mantovani Borba"/>
+            <strong>Caio Mantovani Borba</strong>
+            <span><FiClock /> 08:00</span>
+          </div>
+        </NextAppointment>
+      </Schedule>
+      <Calendar />
+    </Content>
   </Container>
   );
 };
